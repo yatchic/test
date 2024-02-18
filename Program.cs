fdd332b6ba83d235312dd318b5ebc28f3b8a01c1
Ivan Ryzhkovich <yatchic132@gmail.com>
2024-02-18 22:33:43 +0300

    первая часть программы

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿//первая часть программы
+using System;
+
+Console.Write("Введите количество элементов массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine(" ");
+Console.WriteLine("Вводите только числа или латинские символы , с русскими символами ничего не выводиться.");
+ Console.WriteLine(" ");
+
+string[] arr = new string[size];
+Random rand = new Random();  
